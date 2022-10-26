e6709707c6477c7a788bf81053a62e269417d23c
Dmitry Pavlov <dimapav1985@mail.ru>
2022-10-26 14:39:42 +0700

    fix: fixed bugs in testing

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,4 @@
-// eslint-disable-next-line no-useless-escape
-const REGEX = /^(https?):\/\/([wW]{3})?([\w\-\._~:\/?#\[\]@!$&'\(\)*\+,;=])*/;
+const REGEX = /^(https?):\/\/([wW]{3})?([\w\-._~:/?#[\]@!$&'()*+,;=]*)/;
 const JWT_SECRET = 'eb28135ebcfc17578f96d4d65b6c7871f2c803be4180c165061d5c2db621c51b';
 
 module.exports = { REGEX, JWT_SECRET };
