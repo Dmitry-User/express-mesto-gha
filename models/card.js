6238f2eb0d78aee86d52131d56cc3e5de8115f37
Dmitry Pavlov <dimapav1985@mail.ru>
2022-11-04 12:57:08 +0700

    test new ssh-key

--- models/card.js
+++ models/card.js
@@ -6,9 +6,9 @@ const { Schema, model } = mongoose;
 const cardSchema = new Schema({
   name: {
     type: String,
+    required: true,
     minlength: 2,
     maxlength: 30,
-    required: true,
   },
   link: {
     type: String,
